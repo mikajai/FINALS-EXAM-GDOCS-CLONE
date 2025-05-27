62c9690604d2f644b810dd380b72d89ff9290a3b
mikajai <167161170+mikajai@users.noreply.github.com>
2025-05-27 11:48:29 +0800

    data.sql

--- data.sql
+++ data.sql
@@ -3,42 +3,42 @@ CREATE TABLE user_accounts (
 	first_name VARCHAR(255),
 	last_name VARCHAR(255),
 	username VARCHAR(255),
-    password TEXT,
-    is_admin BOOLEAN,
+	password TEXT,
+	is_admin BOOLEAN,
 	is_suspended BOOLEAN,
 	date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
 
 CREATE TABLE documents (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) DEFAULT 'Untitled Document',
-    content TEXT,
-    owner_id INT,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    admin_can_edit TINYINT(1) DEFAULT 0,
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(255) DEFAULT 'Untitled Document',
+	content TEXT,
+	owner_id INT,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	admin_can_edit TINYINT(1) DEFAULT 0,
 );
 
 CREATE TABLE document_permissions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    document_id INT,
-    user_id INT,
-    can_edit TINYINT(1) DEFAULT 1,
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	document_id INT,
+	user_id INT,
+	can_edit TINYINT(1) DEFAULT 1,
 );
 
 CREATE TABLE messages (
-    message_id INT AUTO_INCREMENT PRIMARY KEY,
-    document_id INT NOT NULL,
-    user_id INT NOT NULL,
-    message TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	message_id INT AUTO_INCREMENT PRIMARY KEY,
+	document_id INT NOT NULL,
+	user_id INT NOT NULL,
+	message TEXT NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 
 CREATE TABLE activity_logs (
-    logs_id INT AUTO_INCREMENT PRIMARY KEY,
-    document_id INT,
-    user_id INT,
-    action_type VARCHAR(50),
-    change_summary TEXT,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+	logs_id INT AUTO_INCREMENT PRIMARY KEY,
+	document_id INT,
+	user_id INT,
+	action_type VARCHAR(50),
+	change_summary TEXT,
+	timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
 );
